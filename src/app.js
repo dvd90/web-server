0c62a183273171bf8098558f59e04dc20e55289c
David Sellam <dvdsellam@gmail.com>
2019-12-06 16:49:15 +0200

    deply

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicPath = path.join(__dirname, "../public");
